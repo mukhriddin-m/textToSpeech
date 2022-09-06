b372479290d678597beb47ae923f1ad7e8f6ecc8
mukhriddin-m <mukhriddinmeliev@gmail.com>
2022-09-06 16:40:51 +0500

    edited

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-// var btn = document.getElementById("btn");
-// btn.addEventListener("click", (ea) => {
-//   btn.classList.add("fade");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", (ea) => {
+  btn.classList.add("fade");
 
-//   let recognition = new webkitSpeechRecognition();
-//   recognition.lang = "en-GB";
-//   recognition.addEventListener("result", (e) => {
-//     document.getElementById("text").value = e.results[0][0].transcript;
-//     btn.classList.remove("fade");
-//   });
-//   recognition.start();
-// });
+  let recognition = new webkitSpeechRecognition();
+  recognition.lang = "en-GB";
+  recognition.addEventListener("results", (e) => {
+    document.getElementById("text").value = e.results[0][0].transcript;
+      btn.classList.remove("fade");
+    document.execCommand(text)
+  });
+  recognition.start();
+});
 
